f0d81a0549cc0005da6c4622eee8e5b50188ea09
jakemakesgames <jakebrown.designer@gmail.com>
2020-08-04 19:05:23 +1000

    implemented screenshake

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -8,10 +8,23 @@ public class PlayerHealth : MonoBehaviour
     public GameObject[] bloodstains;
     public GameObject bloodFX;
 
+    Shake shake;
+
+    /// <summary>
+    /// Start is called on the frame when a script is enabled just before
+    /// any of the Update methods is called the first time.
+    /// </summary>
+    void Start()
+    {
+        shake = GameObject.FindGameObjectWithTag("Shake").GetComponent<Shake>();
+    }
+
     void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.tag == "Hazard")
         {
+            shake.CamShake();
+
             int rand = Random.Range(0, bloodstains.Length);
 
             Instantiate(bloodstains[rand], transform.position, transform.rotation);
---
+++ Assets/Scripts/Shake.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shake : MonoBehaviour
+{
+    public Animator camAnim;
+
+    public void CamShake()
+    {
+        int rand = Random.Range(0, 4);
+
+        if (rand == 0)
+        {
+            camAnim.SetTrigger("shake1");
+        } else if (rand == 1)
+        {
+            camAnim.SetTrigger("shake2");
+        } else if (rand == 2)
+        {
+            camAnim.SetTrigger("shake3");
+        } else if (rand == 3)
+        {
+            camAnim.SetTrigger("shake4");
+        } else if (rand == 4)
+        {
+            camAnim.SetTrigger("shake5");
+        }
+    }
+}
