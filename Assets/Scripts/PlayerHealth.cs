56702d355f7018aefc870de3cb6dce279b0da381
jakemakesgames <jakebrown.designer@gmail.com>
2020-08-04 18:41:38 +1000

    created different types of saw hazards, added bloodstains and blood particles

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -5,12 +5,21 @@ using UnityEngine;
 public class PlayerHealth : MonoBehaviour
 {
     public GameObject respawnPoint;
+    public GameObject[] bloodstains;
+    public GameObject bloodFX;
 
     void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.tag == "Hazard")
         {
+            int rand = Random.Range(0, bloodstains.Length);
+
+            Instantiate(bloodstains[rand], transform.position, transform.rotation);
+            GameObject GFX = Instantiate(bloodFX, transform.position, transform.rotation) as GameObject;
+
             transform.position = respawnPoint.transform.position;
+
+            Destroy(GFX, 5f);
         }
     }
 }
